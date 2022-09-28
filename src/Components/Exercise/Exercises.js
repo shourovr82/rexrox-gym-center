5cd7020a60e1576c8440828919b9b564ec65eb9b
shourovr82 <shafinur512@gmail.com>
2022-09-29 04:44:56 +0600

    font family changed

--- src/Components/Exercise/Exercises.js
+++ src/Components/Exercise/Exercises.js
@@ -8,11 +8,11 @@ const Exercise = ({ exercise, addToList }) => {
 
   return (
     <div className="col">
-      <div className="card">
+      <div className="card shadow-3 border-0">
         <img src={picture} className="card-img-top img-fluid p-3 rounded" alt="" />
         <div className="card-body py-0">
           <h6 className="card-title">{name}</h6>
-          <p className='text-muted age m-1'>{description ? description.slice(0, 100) : 'not available'}</p>
+          <p className='text-muted age m-1'>{description ? description.slice(0, 105) : 'not available'}</p>
           <p className='fw-semibold age text-muted m-1'>For Age : {age}</p>
           <p className='fw-semibold age text-muted m-1'>Time Required: {time}s</p>
           <button onClick={() => addToList(exercise)} className='btn btn-success w-100 mt-2 mb-3'>Add to List</button>
