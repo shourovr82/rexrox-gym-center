bf85554e7433b3b7ff395e82b7806b78b093aa68
shourovr82 <shafinur512@gmail.com>
2022-09-29 05:12:46 +0600

    footer and text added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Blog from './Components/Blog/Blog';
+import Footer from './Components/Footer/Footer';
 import Home from './Components/Home/Home';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className='bg-success  bg-opacity-10'>
       <Home></Home>
       <Blog></Blog>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='bg-dark mt-5 py-5'>
+      <p className=' text-center text-white-50'>Created by SHAFIN | shourovr82 </p>
+    </div>
+  );
+};
+
+export default Footer;
